8d98781ee4e0038773ca05b749890e1bbfe65cb0
Alex Habich <me@alexnader.dev>
2020-12-05 23:19:55 -0600

    Edit day 6 formatting

--- 2020/prob06/solution.rs
+++ 2020/prob06/solution.rs
@@ -4,22 +4,22 @@ const INPUT: &[&[&[u8]]] = include!("input");
 
 fn main() {
     let any_yes_count = INPUT.iter()
-        .map(|g|
+        .map(|g| {
             g.iter()
                 .flat_map(|person| person.iter())
                 .unique()
                 .count()
-        )
+        })
         .sum::<usize>();
 
     let all_yes_count = INPUT.iter()
-        .map(|g|
+        .map(|g| {
             g.iter()
                 .flat_map(|person| person.iter())
                 .unique()
                 .filter(|ans| g.iter().all(|person| person.contains(ans)))
                 .count()
-        )
+        })
         .sum::<usize>();
 
     println!("Part A: {}", any_yes_count);
