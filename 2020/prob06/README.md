b6eb00e52445b7c7882ccb14f44347cf13bf1e46
Alex Habich <me@alexnader.dev>
2020-12-05 23:19:00 -0600

    Finish day 6

---
+++ 2020/prob06/README.md
@@ -0,0 +1,6 @@
+# Day 6 Thoughts
+
+Extremely simple problem today. Unfortunately, I ran into a ton of trouble
+formatting the input, so I spent most of my time doing that rather than
+solving the problem. Rust's super powerful and easy iterators (with the
+help of itertools) made solving this problem a breeze.
---
+++ 2020/prob06/solution.rs
@@ -0,0 +1,27 @@
+use itertools::Itertools;
+
+const INPUT: &[&[&[u8]]] = include!("input");
+
+fn main() {
+    let any_yes_count = INPUT.iter()
+        .map(|g|
+            g.iter()
+                .flat_map(|person| person.iter())
+                .unique()
+                .count()
+        )
+        .sum::<usize>();
+
+    let all_yes_count = INPUT.iter()
+        .map(|g|
+            g.iter()
+                .flat_map(|person| person.iter())
+                .unique()
+                .filter(|ans| g.iter().all(|person| person.contains(ans)))
+                .count()
+        )
+        .sum::<usize>();
+
+    println!("Part A: {}", any_yes_count);
+    println!("Part B: {}", all_yes_count);
+}
